4de75eabeaa9c9e5beb95d2d2cf0ecd51a118e7d
Boris De Vloed <boris.de.vloed@gmail.com>
2021-04-30 11:09:02 +0200

    Add turtlefile bean (#3)
    * Add turtlefile bean
    
    * renamed to FileDataObject

---
+++ src/main/java/mu/semte/ch/lib/dto/FileDataObject.java
@@ -0,0 +1,25 @@
+package mu.semte.ch.api.kalliope.lib.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class FileDataObject {
+  private String graph;
+  private String physicalId;
+  private String logicalId;
+  private String created;
+  private String creator;
+  private String physicalFile;
+  private String logicalFile;
+  private String physicalFileName;
+  private String fileSize;
+  private String logicalFileName;
+  private String fileExtension;
+  private String contentType;
+}
